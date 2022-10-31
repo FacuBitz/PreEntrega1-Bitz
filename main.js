242733461a63e49b4b7f1be6e51805ab5b9a0a06
FacuBitz <facubitz01@gmail.com>
2022-10-31 13:08:23 -0300

    dom y storage

--- main.js
+++ main.js
@@ -1,92 +1,72 @@
-const productos = [
-    { nombre: "shampoo", precio: 1500},
-    { nombre: "cera", precio: 800},
-    { nombre: "guante", precio: 500},
-];
-
-function buscarPrecio(productos){
-    return productos.nombre == producto;
-}
-let carrito = []
-
-let seleccion = prompt("*****CARRITO DE COMPRAS*****\nBienvenido! Desea comprar algun producto?\nPresione 'si' para ver el catalogo o 'no' para salir.").toLowerCase();
-
-while (seleccion != "si" && seleccion != "no"){
-    alert("Por favor ingrese si o no para continuar")
-    seleccion = prompt("*****CARRITO DE COMPRAS*****\nBienvenido! Desea comprar algun producto?\nPresione 'si' para ver el catalogo o 'no' para salir.").toLowerCase();
+let totalCarrito;
+let contenedor = document.getElementById("misProd");
+let botonFinalizar = document.getElementById("finalizar");
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+if(carrito.length != 0){
+    console.log ("Recuperando carrito, no ha finalizado su compra!")
+    recuperarTabla();
 }
 
-if (seleccion == "si"){
-    alert("A continuacion nuestro catalogo...")
-    let catalogo = productos.map((producto) => producto.nombre+" "+"$"+producto.precio);
-    alert(catalogo.join("\n"))
-    do{
-        producto = prompt("Agregue el nombre del producto que quiere comprar\nPresione 1 para salir").toLowerCase();
-            if(producto!="1"){
-                switch (producto){
-                    case "shampoo":
-                        precio = productos.find(buscarPrecio).precio;
-                        console.log("shampoo = $"+precio);
-                        break;
-                    case "cera":
-                        precio = productos.find(buscarPrecio).precio;
-                        console.log("cera = $"+precio);
-                        break;
-                    case "guante":
-                        precio = productos.find(buscarPrecio).precio;
-                        console.log("guante = $"+precio);
-                        break;
-                    default:
-                        console.log("Producto sin stock");
-                        precio = 0;
-                        break;
-                }
-                if(precio != 0){
-                    let unidades = parseInt(prompt("Ingrese cantidad de unidades"));
-                    if(isNaN(unidades)){
-                        alert("Ingrese un numero valido!")
-                        console.log("Cantidad no definida")
-                    }else{
-                        carrito.push({producto, unidades, precio});
-                        console.log(carrito);
-                    }
-                }else{
-                    alert("Producto no encontrado")
-                }
-            }
-    } while (producto!="1")
-    carrito.forEach((carritoFinal) => {
-        console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades},
-        total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+function recuperarTabla(){
+    for(const producto of carrito){
+        document.getElementById("tablabody").innerHTML += `
+        <tr>
+            <td>${producto.id}</td>
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+        </tr>
+    `;
+    }
+    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText="Total a pagar $: "+totalCarrito;
+}
+
+function renderizarProd(){
+    for (const producto of productos){
+        contenedor.innerHTML += `
+        <div class="card productos">
+            <img src="${producto.foto}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h4 class="card-title">${producto.nombre}</h4>
+                <p class="card-text h5">$ ${producto.precio}</p>
+                <button id="btn${producto.id}" class="btn btn-primary mt-2">Comprar</button>
+            </div>
+        </div>
+        `;
+    }
+    productos.forEach(producto => {
+        document.getElementById(`btn${producto.id}`).addEventListener("click", function(){
+            agregarAlCarrito(producto);
+        });
     })
-
-    const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
-    console.log("El total a pagar por su compra es: "+total);
-
-    function sumarIva(precio){
-        return precio * 1.21;
-    }
-    let precioConIva = sumarIva(total);
-    console.log("El precio final con iva es: $"+precioConIva);
-    
-    function descontar(precio){
-        return precio * 0.90;
-    }
-    let precioDescuento = descontar(precioConIva);
-    
-    let metodoPago = prompt("Que metodo de pago quiere utilizar?\n1-Efectivo\n2-Tarjeta de credito\nAbonando en efectivo tiene un 10% de descuento."); 
-    while (metodoPago != 1 && metodoPago != 2){
-        alert("Por favor ingrese 1 o 2 para continuar")
-        metodoPago = prompt("Que metodo de pago quiere utilizar?\n1-Efectivo\n2-Tarjeta de credito\nAbonando en efectivo tiene un 10% de descuento.");
-    }
-    if(metodoPago == "1"){
-        precioFinal = precioDescuento;
-    }else{
-        precioFinal = precioConIva;
-    }
-    console.log("El total a abonar es de $ "+Math.round(precioFinal));
-    alert("Gracias por su compra!");
-
-} else if (seleccion == "no"){
-    alert("Gracias por venir, vuelva pronto!")
 }
+
+renderizarProd();
+
+function agregarAlCarrito(productoComprado){
+    carrito.push(productoComprado);
+    console.table(carrito)
+    alert("Producto: "+productoComprado.nombre+" agregado al carrito!");
+    document.getElementById("tablabody").innerHTML += `
+        <tr>
+            <td>${productoComprado.id}</td>
+            <td>${productoComprado.nombre}</td>
+            <td>${productoComprado.precio}</td>
+        </tr>
+    `;
+    totalCarrito = carrito.reduce((acumulador,producto) => acumulador + producto.precio, 0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText = "Total a pagar: $"+totalCarrito;
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+botonFinalizar.onclick = () => {
+    carrito = [];
+    document.getElementById("tablabody").innerHTML="";
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText="Total a pagar $: ";
+
+    localStorage.removeItem("carrito");
+    }
---
+++ productos.js
@@ -0,0 +1,48 @@
+const productos = [{
+        id: 1,
+        foto: "../assets/img/shampooVioleta.png",
+        nombre: "Shampoo ph neutro",
+        precio: 1500
+    },
+    {
+        id: 2,
+        foto: "../assets/img/cera.png",
+        nombre: "Cera de carnauba",
+        precio: 800
+    },
+    {
+        id: 3,
+        foto: "../assets/img/guante.png",
+        nombre: "Guante de microfibra",
+        precio: 500
+    },
+    {
+        id: 4,
+        foto: "../assets/img/silicona.png",
+        nombre: "Silicona para cubiertas",
+        precio: 1200
+    },
+    {
+        id: 5,
+        foto: "../assets/img/hidratanteCuero.png",
+        nombre: "Hidratante de cueros",
+        precio: 700
+    },
+    {
+        id: 6,
+        foto: "../assets/img/shampooDorado.png",
+        nombre: "Shampoo con cera",
+        precio: 2300
+    },
+    {
+        id: 7,
+        foto: "../assets/img/microfibra.png",
+        nombre: "Microfibra de secado",
+        precio: 600
+    },
+    {
+        id: 8,
+        foto: "../assets/img/detailer.png",
+        nombre: "Quick detailer",
+        precio: 700
+    }]
