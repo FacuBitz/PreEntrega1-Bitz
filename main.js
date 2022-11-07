755c94366e05069b916441ad4f1466e3ef5204de
FacuBitz <facubitz01@gmail.com>
2022-11-07 20:11:41 -0300

    btn sumar y restar cantidades

--- main.js
+++ main.js
@@ -7,19 +7,25 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 if(carrito.length != 0){
     console.log ("Recuperando carrito, no ha finalizado su compra!")
     recuperarTabla();
+    console.table(carrito);
 }
 
 function recuperarTabla(){
     for(const producto of carrito){
         document.getElementById("tablabody").innerHTML += `
-        <tr  id="id${producto.id}">
+        <tr id="id${producto.id}">
             <td><img src="${producto.foto}" width="40px" height="40px" alt="..."></td>
             <td class="align-middle">${producto.nombre}</td>
+            <td class="align-middle restar" id="restar${producto.id}"> - </td>
+            <td class="align-middle" id="cantidad${producto.id}">${producto.cantidad}</td>
+            <td class="align-middle sumar" id="sumar${producto.id}"> + </td>
             <td class="align-middle">${producto.precio}</td>
             <td class="align-middle"><button type="button" class="btn" id="eliminar${producto.id}">x</button></td>
         </tr>
     `;
     }
+    sumarr();
+    restarr();
     totalAPagar();
     eliminar();
 }
@@ -46,39 +52,96 @@ function renderizarProd(){
 
 renderizarProd();
 
+
+// recorre el carrito para detectar si esta repetido, si encuentra un id repetido crea un nuevo array,
+// verifica denuevo el id repetido e incrementa su cantidad, 
+// sin el .map cuando reinicio la pagina e intento agregar un producto repetido
+// vuelve a tomar las cantidades del products.js 
 function agregarAlCarrito(producto){
-    carrito.push(producto);
-    console.table(carrito);
-    alert("Producto: "+producto.nombre+" agregado al carrito!");
-    document.getElementById("tablabody").innerHTML += `
+    const repetido = carrito.some((el) => el.id === producto.id);
+    if (repetido) {
+        carrito.map((prod) => {
+            if (prod.id === producto.id) {
+                prod.cantidad++;
+                console.table(carrito);
+                document.getElementById(`cantidad${prod.id}`).innerHTML = `
+                <td class="align-middle" id="cantidad${prod.id}">${prod.cantidad}</td>
+                `;
+            }
+        });
+    } else {
+        carrito.push(producto);
+        console.table(carrito);
+        // alert("Producto: "+producto.nombre+" agregado al carrito!");
+        document.getElementById("tablabody").innerHTML += `
         <tr id="id${producto.id}">
             <td><img src="${producto.foto}" width="40px" height="40px" alt="..."></td>
             <td class="align-middle">${producto.nombre}</td>
+            <td class="align-middle restar" id="restar${producto.id}"> - </td>
+            <td class="align-middle" id="cantidad${producto.id}">${producto.cantidad}</td>
+            <td class="align-middle sumar" id="sumar${producto.id}"> + </td>
             <td class="align-middle">${producto.precio}</td>
             <td class="align-middle"><button type="button" class="btn" id="eliminar${producto.id}">x</button></td>
         </tr>
     `;
+    sumarr();
+    restarr();
+    eliminar();
+    }
 
     localStorage.setItem("carrito", JSON.stringify(carrito));
-
+    
     totalAPagar();
-    eliminar();
+}
+
+function restarr(){
+    for (const producto of carrito){
+        let restar = document.getElementById(`restar${producto.id}`);
+        restar.addEventListener("click", () => {
+            if (producto.cantidad !== 1) {
+                producto.cantidad--;
+                console.table(carrito);
+            }
+            document.getElementById(`cantidad${producto.id}`).innerHTML = `
+                <td class="align-middle" id="cantidad${producto.id}">${producto.cantidad}</td>
+            `;
+
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            totalAPagar();
+        });
+    }
+}
+
+function sumarr(){
+    for (const producto of carrito){
+        let sumar = document.getElementById(`sumar${producto.id}`);
+        sumar.addEventListener("click", () => {
+            producto.cantidad++;
+            console.table(carrito);
+            document.getElementById(`cantidad${producto.id}`).innerHTML = `
+                <td class="align-middle" id="cantidad${producto.id}">${producto.cantidad}</td>
+            `;
+
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            totalAPagar();
+        });
+    }
 }
 
 function totalAPagar(){
-    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio * producto.cantidad,0);
     infoTotal.innerText="Total a pagar: $"+totalCarrito;
 }
 
 function eliminar(){
-    for (const producto of carrito){
-        productoEliminado = document.getElementById(`eliminar${producto.id}`).addEventListener("click", function(){
+        for (const producto of carrito){
+            productoEliminado = document.getElementById(`eliminar${producto.id}`).addEventListener("click", function(){
                 eliminarDelCarrito(producto);
                 document.getElementById(`id${producto.id}`).remove();
                 totalAPagar();
                 localStorage.setItem("carrito", JSON.stringify(carrito));
             });
-        }
+    }
 }
 
 function eliminarDelCarrito(productoEliminado){
@@ -88,10 +151,12 @@ function eliminarDelCarrito(productoEliminado){
 }
 
 botonFinalizar.onclick = () => {
-    carrito = [];
-    document.getElementById("tablabody").innerHTML="";
-    let infoTotal = document.getElementById("total");
-    infoTotal.innerText="Total a pagar: $";
+    // if (carrito.lenght != 0){
+        carrito = [];
+        document.getElementById("tablabody").innerHTML="";
+        let infoTotal = document.getElementById("total");
+        infoTotal.innerText="Total a pagar: $";
 
-    localStorage.removeItem("carrito");
+        localStorage.removeItem("carrito");
+    // }
     }
--- productos.js
+++ productos.js
@@ -2,47 +2,55 @@ const productos = [{
         id: 1,
         foto: "./assets/img/shampooVioleta.png",
         nombre: "Shampoo ph neutro",
-        precio: 1500
+        precio: 1500,
+        cantidad: 1
     },
     {
         id: 2,
         foto: "./assets/img/cera.png",
         nombre: "Cera de carnauba",
-        precio: 800
+        precio: 800,
+        cantidad: 1
     },
     {
         id: 3,
         foto: "./assets/img/guante.png",
         nombre: "Guante de microfibra",
-        precio: 500
+        precio: 500,
+        cantidad: 1
     },
     {
         id: 4,
         foto: "./assets/img/silicona.png",
         nombre: "Silicona para cubiertas",
-        precio: 1200
+        precio: 1200,
+        cantidad: 1
     },
     {
         id: 5,
         foto: "./assets/img/hidratanteCuero.png",
         nombre: "Hidratante de cueros",
-        precio: 700
+        precio: 700,
+        cantidad: 1
     },
     {
         id: 6,
         foto: "./assets/img/shampooDorado.png",
         nombre: "Shampoo con cera",
-        precio: 2300
+        precio: 2300,
+        cantidad: 1
     },
     {
         id: 7,
         foto: "./assets/img/microfibra.png",
         nombre: "Microfibra de secado",
-        precio: 600
+        precio: 600,
+        cantidad: 1
     },
     {
         id: 8,
         foto: "./assets/img/detailer.png",
         nombre: "Quick detailer",
-        precio: 700
+        precio: 700,
+        cantidad: 1
     }]
